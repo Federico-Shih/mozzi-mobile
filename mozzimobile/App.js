3195d42d9ccc30ba622d0353d9a10cc994877f08
Federico Shih <fedeshih@gmail.com>
2019-06-03 17:11:43 -0300

    Started working on prop manipulation

--- mozzimobile/App.js
+++ mozzimobile/App.js
@@ -7,7 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, Text, View} from 'react-native';
+import styles from './Styles.js';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,33 +18,24 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+
+class Title extends Component<Props> {
+  render() {
+    <View>
+      <Text style={{fontSize: 40},styles.title}>
+        {this.props.text}  
+      </Text>
+    </View>
+  }
+}
+
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Title text = "Mozzi"/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
