a6eb19731dc438055bd12525da32ea759c2cf819
Federico-Shih <fedeshih@gmail.com>
2019-06-06 14:15:07 -0300

    Added react-native.cmd to debug from ORT computers

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mozzi-mobile
+
+Para poder correr el proyecto react native en las computadoras de ORT:
+Correr set_env para definir ANDROID_HOME
+Hacer   Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+para instalar chocolatey
+Usar chocolatey para instalar jdk8
+instalar react-native-cli como global
+usar .\react-native en vez de react-native
--- set_env.bat
+++ set_env.bat
@@ -1,9 +1,11 @@
 @echo off
 setlocal 
-  set ENVVAR=ANDROID_HOME && C:\Users\%username%\AppData\Local\Android\Sdk
-  SETX /M ANDROID_HOME "C:\Users\%username%\AppData\Local\Android\Sdk"
-  set PATH = %path%; C:\Users\%username%\AppData\Local\Android\Sdk\platform-tools
-  SETX /M PATH "%path%; C:\Users\%username%\AppData\Local\Android\Sdk\platform-tools"
-  set PATH = %path%; C:\\Users\%username%\\AppData\\Roaming\\npm
-  SETX /M PATH "%path%; C:\\Users\\%username%\\AppData\\Roaming\\npm"
+  set ENVVAR=ANDROID_HOME && C:\Android\Sdk
+  SETX ANDROID_HOME "C:\Android\Sdk"
+  Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  pause
+  set PATH = %PATH%;C:\Android\Sdk\platform-tools
+  setx "%PATH%;C:\Android\Sdk\platform-tools"
+  set PATH = %PATH%;C:\Users\%username%\AppData\Roaming\npm
+  setx "%PATH%;C:\Users\%username%\AppData\Roaming\npm"
 endlocal
