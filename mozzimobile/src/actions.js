070dc7b79ccce68b45b36398abf673896c4d10f3
Federico.Shih <fedeshih@gmail.com>
2019-06-19 00:06:06 -0300

    Moving componenets and starting with register page

--- mozzimobile/src/actions.js
+++ mozzimobile/src/actions.js
@@ -1,9 +1,25 @@
 export const CHANGE_PAGE = 'CHANGE_PAGE';
 export const ADD_PERMISSIONS = 'ADD_PERMISSIONS';
 
-export function changePage(currentPage) {
-    return {
-        type: CHANGE_PAGE,
-        currentPage
-    }
-}
+export const ADD_LOGIN = 'ADD_LOGIN';
+
+export const LOGIN_JSON = {
+    type: ADD_LOGIN,
+    form: {
+        username:'',
+        password: ''
+    }    
+};
+
+export const ADD_REGISTER = 'ADD_REGISTER';
+
+export const REGISTER_JSON = {
+    type: ADD_REGISTER,
+    form: {
+        username: '',
+        date: '',
+        email: '',
+        password: '',
+    }  
+};
+
--- mozzimobile/src/pages/register.js
+++ mozzimobile/src/pages/register.js
@@ -1,23 +1,74 @@
-import {Text, View} from 'react-native';
+import {Text, View, TextInput} from 'react-native';
 import React, {Component} from 'react';
-import styles from '../styles/styles';
-import {Title, Button} from '../props';
+import styles from '../libraries/styles/styles';
+import {Title, Button} from '../libraries/props';
+import {ADD_REGISTER, REGISTER_JSON} from '../actions';
+import DatePicker from 'react-native-datepicker'
+
 
 type Props = {};
+const date = (new Date()).toISOString().split('T')[0];
 
 export default class Register extends Component<Props> {
-    render() {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.textTest}>
-                Register
-          </Text>
-           <View style = {{alignSelf: 'stretch', marginBottom: '0%', justifyContent: 'flex-end', flex: 1}}>         
-              <Button styles={styles.backButton} text="Back" onPress = {()=> { this.props.navigation.goBack()}} />
-           </View>
-        </View>
+    constructor(props){
+        super(props);
+    }
 
-      );
+    state = {
+        username: '',
+        date: date,
+        email: '',
+        password: ''
+    };
+    
+    render() {
+        return (
+                <View style={styles.container}>
+                    <View style = {{flex: 1}}>
+                        <Text style={styles.textTest}>
+                            Register
+                        </Text>
+                    </View>
+    
+                    <View style = {{width: '80%', alignContent:'center', alignItems: 'center', flex: 4}}>
+                        <TextInput 
+                            style= {{width: '80%', backgroundColor: '#FFFFFF', margin: 20}} 
+                            placeholder= 'Username' 
+                            onChangeText={(text) => {this.setState({username: text})}}
+                            value = {this.state.username}
+                            />
+                        <DatePicker
+                            style={{width: '80%'}}
+                            date={this.state.date}
+                            mode="date"
+                            placeholder="select birthday"
+                            format="YYYY-MM-DD"
+                            maxDate= {date}
+                            confirmBtnText="Confirm"
+                            cancelBtnText="Cancel"
+                            customStyles={{
+                                dateIcon: {
+                                    position: 'absolute',
+                                    left: 0,
+                                    top: 4,
+                                    marginLeft: 0
+                                },
+                                dateInput: {
+                                        marginLeft: 0
+                                }
+                            }} 
+                            onDateChange={(date) => {this.setState({date: date})}}
+
+                            />
+                        <TextInput keyboardType='email-address' style= {{width: '80%', backgroundColor: '#FFFFFF', margin: 20}} placeholder= 'e-mail address' />
+                        <TextInput secureTextEntry= {true} style= {{width: '80%', backgroundColor: '#FFFFFF'}} placeholder= 'password' />
+
+                    </View>
+                    <View style = {{alignSelf: 'stretch', marginBottom: '0%', justifyContent: 'flex-end', flex: 1}}>         
+                        <Button styles={styles.backButton} text="Back" onPress = {()=> { this.props.navigation.goBack()}} />
+                    </View>
+                </View>
+        );
     }
 }
 
