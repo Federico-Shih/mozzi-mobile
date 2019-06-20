991f9f98cc9740f97f0c8cf067d5e588fc2d3513
Federico.Shih <fedeshih@gmail.com>
2019-06-20 03:12:50 -0300

    Still still working on register

--- mozzimobile/src/pages/register.js
+++ mozzimobile/src/pages/register.js
@@ -1,14 +1,19 @@
 import {Text, View, Dimensions} from 'react-native';
 import React, {Component} from 'react';
 import styles from '../libraries/styles/styles';
-import {Title, Button, InputWithIcon, StyledTitle} from '../libraries/props';
+import {Title, InputWithIcon, StyledTitle} from '../libraries/props';
 import { LOGGING_IN } from '../actions';
 import {connect} from 'react-redux';
-import { Input } from 'react-native-elements';
+import { Input, Button} from 'react-native-elements';
 
 
 type Props = {};
 const date = (new Date()).toISOString().split('T')[0];
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
 
 const InputTextWidth = (Math.round((1 - 60/Dimensions.get('window').width)*100)).toString() + '%';
 
@@ -17,47 +22,106 @@ class Register extends Component<Props> {
         super(props);
     }
 
+    //ACORDATE DE MOVER ESTOS ESTILOS A STYLESHEET ASI NO SE VUELVE REPETITIVO
     state = {
         name: '',
         surname: '',
-        date: date,
         email: '',
-        password: ''
-    };
+        password: '',
+        tempConfirmedPassword: '',
+        style: {
+            name: {borderBottomWidth: 2, borderWidth: 2, borderRadius: 30, borderColor: 'black', marginVertical: 7},
+            surname: {borderBottomWidth: 2, borderWidth: 2, borderRadius:30, borderColor:'black', marginVertical:7},
+            email: {borderBottomWidth: 2, borderWidth: 2, borderRadius:30, borderColor:'black', marginVertical:7},
+            password: {borderBottomWidth: 2, borderWidth: 2, borderRadius:30, borderColor:'black', marginVertical:7},
+            confirmpassword: {borderBottomWidth: 2, borderWidth: 2, borderRadius:30, borderColor:'black', marginVertical:7}
+        }
+    }; 
     
+    checkIfValidEmailAndSet = (text = this.state.email) => {
+        if(validateEmail(text)) {
+            this.setState({email: text});
+            this.setState({style: {...this.state.style, email: {...this.state.style.email, borderColor:'black'}}});
+        } else {
+            this.setState({style: {...this.state.style, email: {...this.state.style.email, borderColor:'red'}}});
+        }
+    };
 
+    checkIfValidPasswordAndSet = (text = this.state.password) => {
+        if(text.length >= 8) {
+            this.setState({password: text});
+            this.setState({style: {...this.state.style, password: {...this.state.style.password, borderColor:'black'}}});
+        } else {
+            this.setState({style: {...this.state.style, password: {...this.state.style.password, borderColor:'red'}}});
+        }
+    };
+
+    checkIfPasswordsMatchAndSet = (t1 = this.state.password, t2 = this.state.tempConfirmedPassword) => {
+        if(t1 === t2) {
+            this.setState({password: t1});
+            this.setState({style: {...this.state.style, confirmpassword: {...this.state.style.confirmpassword, borderColor:'black'}}});
+        } else {
+            this.setState({style: {...this.state.style, confirmpassword: {...this.state.style.confirmpassword, borderColor:'red'}}});
+        }
+    }
 
     render() {
         return (
                 <View style={styles.container}>
                     <StyledTitle text='Registrate' style={styles.logregTitle} />
-    
+
                     <View style = {{width: InputTextWidth, alignContent:'center', alignItems: 'center', flex: 10, marginTop: '5%'}}>
                         <Input 
-                            style= {styles.inputText} 
                             placeholder= 'Name' 
                             onChangeText={(text) => {this.setState({name: text})}}
                             value = {this.state.name}
+                            inputContainerStyle={this.state.style.name}
+                            leftIcon = {{ type: 'material', name: 'account-circle'}} 
                             />
+
                         <Input 
-                            style= {styles.inputText} 
                             placeholder= 'Surname' 
                             onChangeText={(text) => {this.setState({surname: text})}}
                             value = {this.state.surname}
+                            inputContainerStyle={this.state.style.surname}
+                            leftIcon = {{ type: 'material', name: 'perm-identity'}} 
                             />
+
                         <Input 
                             keyboardType='email-address' 
-                            style= {styles.inputText} 
-                            placeholder= 'e-mail address' 
+                            placeholder= 'Email'
+                            onChangeText={(text) => {this.setState({email: text})}}
+                            onSubmitEditing= {()=> {this.checkIfValidEmailAndSet()}}
+                            inputContainerStyle={this.state.style.email}
+                            leftIcon = {{ type: 'material', name: 'email'}} 
                             />
+
                         <Input 
                             secureTextEntry= {true} 
-                            style= {styles.inputText} 
-                            placeholder= 'password' 
+                            placeholder= 'Password' 
+                            onChangeText={(text) => {this.setState({password: text})}}
+                            onSubmitEditing= {()=> {this.checkIfValidPasswordAndSet()}}
+                            inputContainerStyle={this.state.style.password}
+                            leftIcon = {{ type: 'material', name: 'lock'}} 
                             />
+                        
+                        <Input 
+                            secureTextEntry= {true} 
+                            placeholder= 'Confirm Password' 
+                            onChangeText={(text) => {this.setState({tempConfirmedPassword: text})}}
+                            onSubmitEditing= {()=> {this.checkIfPasswordsMatchAndSet()}}
+                            inputContainerStyle={this.state.style.confirmpassword}
+                            leftIcon = {{ type: 'material', name: 'lock'}} 
+                            />
+
                     </View>
                     <View style = {{alignSelf: 'stretch', marginBottom: '0%', justifyContent: 'flex-end', flex: 1}}>         
-                        <Button styles={styles.backButton} text="Back" onPress = {()=> { this.props.navigation.goBack()}} />
+                        <Button 
+                            title="Back" 
+                            onPress = {()=> { this.props.navigation.goBack()}} 
+                            type= 'clear'
+                            titleStyle= {{fontSize:25}}
+                            />
                     </View>
                 </View>
         );
