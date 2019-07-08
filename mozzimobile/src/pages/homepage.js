183985e3fa13219eaa6472ab6a6040052f4bd7f2
Federico-Shih <fedeshih@gmail.com>
2019-07-08 16:41:13 -0300

    Finished sidebar menu

--- mozzimobile/src/pages/homepage.js
+++ mozzimobile/src/pages/homepage.js
@@ -1,44 +1,148 @@
-import {Text, View} from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from 'react-native';
 import React, {Component} from 'react';
 import styles from '../libraries/styles/styles';
 import { Input, Button, Header, Icon} from 'react-native-elements';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import SideMenu from 'react-native-side-menu-over';
+ 
 type Props = {};
 
 export default class Register extends Component<Props> {
     state = {
-      
-   }
+        isOpen: false,    
+    };
+
+    toggle () {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        });
+    }
+    
+    updateMenuState(isOpen) {
+        this.setState({
+          isOpen: isOpen,
+        })
+    }
+
     render() {
-      return (
-        <View style={styles.container}>
-            <Header
-                leftComponent=
-                {
-                  <Button 
-                      containerStyle = {{bottom: '50%'}}
-                      icon = {
-                            <Icon
-                                name="menu"
-                                size={30}
-                            />
-                          }
-                      type = 'clear'
-                      onPress = {() => {}}
-                      />
-                }
-                containerStyle= {{height: '7%', backgroundColor: '#F5FCFF', borderWidth: 0}}
-                centerComponent= {{text: 'Mozzi', style: {bottom: '50%', fontSize: 25}}}
-                placement= 'left'
-            />
-            <ScrollView style= {{width: '100%'}}>
-               {
-                  
-               }
-            </ScrollView>
-            
-        </View>
-      );
+          return (
+            <SideMenu 
+                menu={<Menu navigation={this.props.navigation}/>} 
+                isOpen={this.state.isOpen}
+                onChange= {isOpen => this.updateMenuState(isOpen)}
+            >
+                <View style={styles.container}>
+                    <Header
+                        leftComponent=
+                        {
+                          <Button 
+                              containerStyle = {{bottom: '50%'}}
+                              icon = {
+                                    <Icon
+                                        name="menu"
+                                        size={30}
+                                    />
+                                  }
+                              type = 'clear'
+                              onPress = {() => {this.toggle()}}
+                              />
+                        }
+                        containerStyle= {{height: '7%', backgroundColor: '#F5FCFF', borderWidth: 0}}
+                        centerComponent= {{text: 'Mozzi', style: {bottom: '50%', fontSize: 25}}}
+                        placement= 'left'
+                    />
+                    <ScrollView style= {{width: '100%'}}>
+                      {
+                          
+                      }
+                    </ScrollView>
+                </View>  
+            </SideMenu>
+          
+          );
     }
 }
+
+
+function Menu ({navigation}) {
+    return(
+        <ScrollView scrollsToTop = {false} style = {menuStyles.menu}>
+            <View style={menuStyles.avatarContainer}>
+                <Image
+                  style={menuStyles.avatar}
+                  source={{ uri: 'https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png' }}
+                />
+                <Text style={menuStyles.name}>Your name</Text>
+            </View>
+
+            <Text
+              onPress={() => navigation.navigate('Turns')}
+              style={menuStyles.item}
+            >
+              Tus Turnos
+            </Text>
+
+            <Text
+              onPress={() => navigation.navigate('Profile')}
+              style={menuStyles.item}
+            >
+              Mi Perfil
+            </Text>
+
+            <Text
+              onPress={() => navigation.navigate('Recent')}
+              style={menuStyles.item}
+            >
+              Recientes
+            </Text>
+
+            <Text
+              onPress={() => navigation.navigate('Favs')}
+              style={menuStyles.item}
+            >
+              Favoritos
+            </Text>
+
+            <Text
+              onPress={() => navigation.navigate('Config')}
+              style={menuStyles.item}
+            >
+              Configurar
+            </Text>
+        </ScrollView>
+    );
+}
+
+const menuStyles = StyleSheet.create({
+  menu: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  avatarContainer: {
+    marginBottom: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems:'center'
+  },
+  avatar: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+  },
+  name: {
+    left: 20
+  },
+  item: {
+    fontSize: 15,
+    fontWeight: '300',
+    paddingTop: 10,
+  },
+});
